8843565dbc425fc98752c2893da26d39fb5010ff
zeroslee21 <zeros.lee@gmail.com>
2020-02-18 18:37:48 +0900

    2nd step

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Proptypes from "prop-types";
+//import Zeroshome from "./Zeroshome";
+//import logo from './logo.svg';
+//import './App.css';
+
+
+
+const foodILike = [
+  {
+    id: 1,
+    name: "Kimchi",
+    image: "https://www.maangchi.com/wp-content/uploads/2019/11/vegankimchi-insta-1200x1200.jpg",
+    rating: 5
+  },
+  {
+    id: 2,
+    name: "Samgyeopsal",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Samgyeopsal-gui.jpg/440px-Samgyeopsal-gui.jpg",
+    rating: 4.3
+  },
+]
+
+
+
+function Food({name, picture, rating}){
+  //console.log(props.fav);
+  return (<div>
+    <h2>I like {name}</h2>
+    <h4>{rating}/5.0</h4>
+    <img src={picture} alt={name} />
+  </div>
+  );
+}
+
+Food.propTypes = {
+  name: Proptypes.string.isRequired,
+  picture: Proptypes.string.isRequired,
+  rating: Proptypes.number.isRequired,
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodILike.map(dish => (
+      <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />))}
     </div>
   );
 }
---
+++ src/Zeroshome.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Zeroshome(){
+    return <h3>I love zeroshome</h3>;
+}
+
+export default Zeroshome;
